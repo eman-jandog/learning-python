e3e6f8465194e058ffd720757ddb7320addcb0f7
Name-San <eman.jandog@gmail.com>
2025-07-15 21:16:34 +0800

    change library file structue

--- library_system/app/app.py
+++ library_system/app/app.py
@@ -5,7 +5,7 @@ from flask_migrate import Migrate
 db = SQLAlchemy()
 
 def create_app():
-    app = Flask(__name__, template_folder='templates', static_folder='static')
+    app = Flask(__name__, template_folder='templates', static_folder='static', static_url_path='/')
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///./library.db'
 
     db.init_app(app)
